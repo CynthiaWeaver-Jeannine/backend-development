2db15549d1dbfbb568fca6199a8b06520619cc39
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-07-21 05:44:35 -0400

    error handling and utils files

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,8 +1,18 @@
-import React from "react";
+/** @format */
 
+import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+
+//import components
 import Dashboard from "../dashboard/Dashboard";
+import ReservationCreate from "../reservations/ReservationCreate";
+import ReservationSeat from "../reservations/ReservationSeat";
+import ReservationSearch from "../reservations/ReservationSearch";
+import ReservationEdit from "../reservations/ReservationEdit";
+import TableCreate from "../tables/TableCreate";
 import NotFound from "./NotFound";
+
+//import utility functions
 import { today } from "../utils/date-time";
 
 /**
@@ -13,22 +23,41 @@ import { today } from "../utils/date-time";
  * @returns {JSX.Element}
  */
 function Routes() {
-  return (
-    <Switch>
-      <Route exact={true} path="/">
-        <Redirect to={"/dashboard"} />
-      </Route>
-      <Route exact={true} path="/reservations">
-        <Redirect to={"/dashboard"} />
-      </Route>
-      <Route path="/dashboard">
-        <Dashboard date={today()} />
-      </Route>
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route
+				exact={true}
+				path="/">
+				<Redirect to={"/dashboard"} />
+			</Route>
+			<Route
+				exact={true}
+				path="/reservations">
+				<Redirect to={"/dashboard"} />
+			</Route>
+			<Route path="/dashboard">
+				<Dashboard date={today()} />
+			</Route>
+			<Route path="/reservations/new">
+				<ReservationCreate />
+			</Route>
+			<Route path="/reservations/:reservation_id/edit">
+				<ReservationEdit />
+			</Route>
+			<Route path="/reservations/:reservation_id/seat">
+				<ReservationSeat />
+			</Route>
+			<Route path="/tables/new">
+				<TableCreate />
+			</Route>
+			<Route path="/search">
+				<ReservationSearch />
+			</Route>
+			<Route>
+				<NotFound />
+			</Route>
+		</Switch>
+	);
 }
 
 export default Routes;
